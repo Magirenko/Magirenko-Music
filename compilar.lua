9261912980aeabc9d817cebf4824096294da0859
Migue Magic <miguelitoeremenko@gmail.com>
2024-12-07 23:01:31 -0400

    Actualizacion definitiva de compilar.lua

--- compilar.lua
+++ compilar.lua
@@ -1,7 +1,6 @@
 print("Compilador CLI De Magirenko v0.1\n\nSoftware a compilar: Magirenko Music\nNota: Esto solo compilara el software, lo compilado no es igual al producto final en las descargas.\nSi vas a usar el software, se recomienda que uses los de las descargas de los releases.\n\n")
 while true do
   local optimizacion = "Debug"
-  local arquitectura = "win-x86"
   local autocontenido = false
   print("Magirenko Music se compilara en la carpeta bin\nQuieres compilar Magirenko Music? (y/n)")
   local response = io.read()
@@ -16,23 +15,17 @@ while true do
     end
     print("Modo de Optimizacion Seleccionado: " .. optimizacion)
 
-    print("Arquitectura:\na) win-x86 (32 bits)\n(a)")
-    local response3 = io.read()
-    if response3 == "a" then
-      arquitectura = "win-x86"
-    end
-    print("Arquitectura seleccionada: " .. arquitectura)
-
     print("Autocontenido? (y/n)")
-    local response4 = io.read()
-    if response4 == "y" then
+    local response3 = io.read()
+    if response3 == "y" then
       autocontenido = true
-    elseif response4 == "n" then
+    elseif response3 == "n" then
       autocontenido = false
     end
     print("Autocontenido:" .. tostring(autocontenido))
     print("Magirenko Music esta listo para compilarse.\nCompilando...")
-    os.execute("dotnet build --self-contained ".. tostring(autocontenido) .. " --configuration ".. optimizacion .. " --runtime ".. arquitectura)
+    os.execute("dotnet build --self-contained ".. tostring(autocontenido) .. " -c ".. optimizacion)
+    break
   elseif response == "n" then
     break
   end
